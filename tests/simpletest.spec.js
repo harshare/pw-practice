29719f12c5dbef6ff612865f6a42cfeb6fded5c0
nramya984 <nramya984@gmail.com>
2026-04-18 22:58:38 +0100

    simpletest added2

--- tests/simpletest.spec.js
+++ tests/simpletest.spec.js
@@ -1,17 +1,6 @@
-
-
-
 import { test, expect } from '@playwright/test';
 
 test('simple test', async ({page})=>{
-
-
     await page.goto("https://www.saucedemo.com/");
     await expect(page.locator(".login_logo")).toBeVisible();
-
-
-
-
-
-
 })
